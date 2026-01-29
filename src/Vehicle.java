6742eb023ce1a99a7039b614df99ad98b8cb4185
Moneshwaran S <moneshwaran1221@gmail.com>
2026-01-29 12:29:41 +0530

    Create Vehicle.java

---
+++ src/Vehicle.java
@@ -0,0 +1,9 @@
+class Vehicle {
+    String vehicleNumber;
+    String vehicleType;
+
+    Vehicle(String number, String type) {
+        this.vehicleNumber = number;
+        this.vehicleType = type;
+    }
+}
