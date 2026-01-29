051bfc3b11cebfbec67eaedd186972e32ee6444e
Moneshwaran S <moneshwaran1221@gmail.com>
2026-01-29 12:29:13 +0530

    Delete src directory

--- src/Vehicle.java
+++
@@ -1,9 +0,0 @@
-class Vehicle {
-    String vehicleNumber;
-    String vehicleType;
-
-    Vehicle(String number, String type) {
-        this.vehicleNumber = number;
-        this.vehicleType = type;
-    }
-}
