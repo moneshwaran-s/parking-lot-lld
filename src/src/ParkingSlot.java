6895aa962b782a06299a06f48c9cb3b5c62d3e90
Moneshwaran S <moneshwaran1221@gmail.com>
2026-01-29 12:27:13 +0530

    Update ParkingSlot.java

--- src/src/ParkingSlot.java
+++ src/src/ParkingSlot.java
@@ -1,1 +1,21 @@
+class ParkingSlot {
+    int slotNumber;
+    boolean isOccupied;
+    Vehicle vehicle;
+
+    ParkingSlot(int slotNumber) {
+        this.slotNumber = slotNumber;
+        this.isOccupied = false;
+    }
+
+    void parkVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+        this.isOccupied = true;
+    }
+
+    void removeVehicle() {
+        this.vehicle = null;
+        this.isOccupied = false;
+    }
+}
 
